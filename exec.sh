1ee10fcfd1a03f39b9a5b73f40855e6cba2b5bf1
williamjhyland <williamjhyland@outlook.com>
2023-12-08 18:38:44 -0500

    fixed setup venv

--- exec.sh
+++ exec.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
-
-# Grab directory
-current_dir=${PWD}
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+# Create virtual Python environment
 cd `dirname $0`
 
-# Be sure to use `exec` so that termination signals reach the python process,
-# or handle forwarding termination signals manually
+# Create a virtual environment to run our code
+VENV_NAME="venv"
+PYTHON="$VENV_NAME/bin/python"
+ENV_ERROR="This module requires Python >=3.8, pip, and virtualenv to be installed."
 
-# setup our python virtual environment
-source ${SCRIPT_DIR}/bin/activate
+if ! python3 -m venv --system-site-packages $VENV_NAME >/dev/null 2>&1; then
+    echo "Failed to create virtualenv."
+    if command -v apt-get >/dev/null; then
+        echo "Detected Debian/Ubuntu, attempting to install python3-venv automatically."
+        SUDO="sudo"
+        if ! command -v $SUDO >/dev/null; then
+            SUDO=""
+        fi
+		if ! apt info python3-venv >/dev/null 2>&1; then
+			echo "Package info not found, trying apt update"
+			$SUDO apt -qq update >/dev/null
+		fi
+        $SUDO apt install -qqy python3-venv >/dev/null 2>&1
+        if ! python3 -m venv $VENV_NAME >/dev/null 2>&1; then
+            echo $ENV_ERROR >&2
+            exit 1
+        fi
+    else
+        echo $ENV_ERROR >&2
+        exit 1
+    fi
+fi
 
-python ${SCRIPT_DIR}/src/main.py $@
+echo "Virtualenv found/created. Installing/upgrading Python packages..."
+if ! $PYTHON -m pip install -r requirements.txt -Uqq; then
+    exit 1
+fi
 
-# deactivate - if we get here
-deactivate
+echo "Starting module..."
+exec ${SCRIPT_DIR}/venv/bin/python3 ${SCRIPT_DIR}/src/main.py $@
